00172e8892cb1860a15858dedd97ef6406e9188d
Ki <krozin@gmail.com>
2016-10-12 00:49:06 +0300

    fix header

--- pngwrapper.h
+++ pngwrapper.h
@@ -1,7 +1,6 @@
 #ifndef pngwrapper_h__
 #define pngwrapper_h__
 
-
 void abort_(const char * s, ...);
 
 void read_png_file(char* file_name,
@@ -16,15 +15,15 @@ void read_png_file(char* file_name,
 void write_png_file(char* file_name,
                     png_structp* png_ptr,
                     png_infop* info_ptr,
-                    int* width, int* height,
-                    png_byte* color_type,
-                    png_byte* bit_depth,
+                    int width, int height,
+                    png_byte color_type,
+                    png_byte bit_depth,
                     png_bytep* row_pointers);
 
 void process_file(int threshold,
                   png_structp* png_ptr,
                   png_infop* info_ptr, 
-                  int* width, int* height,
+                  int width, int height,
                   png_bytep* row_pointers,
                   char* png_binary_image);
 
