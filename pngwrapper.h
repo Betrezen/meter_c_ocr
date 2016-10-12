3b6afd89c0750319de9e7a70cfc21a21bff9f4c5
Ki <krozin@gmail.com>
2016-10-12 23:38:52 +0300

    fix

--- pngwrapper.h
+++ pngwrapper.h
@@ -1,7 +1,7 @@
 #ifndef pngwrapper_h__
 #define pngwrapper_h__
 
-void abort_(const char * s, ...);
+void abortme(const char * s, ...);
 
 void read_png_file(char* file_name,
                    png_structp* png_ptr,
@@ -13,16 +13,12 @@ void read_png_file(char* file_name,
                    char* png_binary_image);
 
 void write_png_file(char* file_name,
-                    png_structp* png_ptr,
-                    png_infop* info_ptr,
                     int width, int height,
                     png_byte color_type,
                     png_byte bit_depth,
                     png_bytep* row_pointers);
 
 void process_file(int threshold,
-                  png_structp* png_ptr,
-                  png_infop* info_ptr, 
                   int width, int height,
                   png_bytep* row_pointers,
                   char* png_binary_image);
